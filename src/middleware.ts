d336580028e195d354364bd5f1ad0c7d89a00f5a
yakirco13-cloud <yakirco13@gmail.com>
2025-12-26 09:39:08 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -34,7 +34,8 @@ export async function middleware(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/signup') &&
+    !request.nextUrl.pathname.startsWith('/register') &&
+    !request.nextUrl.pathname.startsWith('/forgot-password') &&
     !request.nextUrl.pathname.startsWith('/auth')
   ) {
     const url = request.nextUrl.clone()
